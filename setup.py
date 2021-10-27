49db7bc5899ae62cc8dc1309546986584d3f3146
Amit <amit.chawarekar@gmail.com>
2021-10-27 16:11:45 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ USER_NAME = "amit04"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.2",
+    version="0.0.3",
     author=USER_NAME,
     author_email="amit.chawarekar@gmail.com",
     description="its a implementation of perceptron",
